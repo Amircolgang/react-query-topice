144f2c18aac79e765753b76a895959743669d74a
amircolgang <parham38385747@gmail.com>
2025-08-27 13:24:34 -0700

    Amircolgang: changes on 2025-08-27 13:24:34

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-
-const client =  new 
+import { QueryClient , QueryClie }
+ 
+// const client =  new Query 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
