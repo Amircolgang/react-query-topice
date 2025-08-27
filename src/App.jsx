87ccb3f0f2b776cfd29e7326c7a00380fdfbfa1a
amircolgang <parham38385747@gmail.com>
2025-08-27 13:41:15 -0700

    Amircolgang: changes on 2025-08-27 13:41:15

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const result = useQuery({
     queryKey : ["Posts"] ,
     queryFn : async function () {
-      const data = await fetch("https://jsonplaceholder.typicode.com/todos/1") 
+      const data = await fetch("https://jsonplaceholder.typicode.com/comments?postId=1") 
       return data.json()
     }
   })
