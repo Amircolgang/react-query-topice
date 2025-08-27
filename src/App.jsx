6f2023b8da3966a86ea8362966e0f1b25efb36dc
amircolgang <parham38385747@gmail.com>
2025-08-27 13:30:52 -0700

    Amircolgang: changes on 2025-08-27 13:30:51

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useQuery({
+    
+  })
+
 
   return (
     <>
